b21920b86bbab6e4b63e8d674b2f99f27c18e702
alex <alex.abakshin3423@gmail.com>
2025-09-21 22:12:10 +0300

    fixed error due getting value from unallocated pointer of 'Player' type

--- srcs/serverCore.c
+++ srcs/serverCore.c
@@ -1606,6 +1606,7 @@ int server_run(Banker* banker, int ls)
 						}
 						else
 						{
+							int left_pl_num = p->number;
 							player_left_game(banker, p, i, &readfds);
 							
 							if ( banker->alive_players == 1 )
@@ -1635,7 +1636,6 @@ int server_run(Banker* banker, int ls)
 								mes_tokens[1] = ap_buf;
 
 								char left_p_num_buf[10];
-								int left_pl_num = p->number;
 								itoa(left_pl_num, left_p_num_buf, 9);
 								mes_tokens[2] = left_p_num_buf;
 
@@ -1651,7 +1651,7 @@ int server_run(Banker* banker, int ls)
 				} /* end if FD_ISSET */
 			} /* end server_banker.pl_array[i] != NULL */
 		} /* end for */
-		
+
 		if ( banker->game_started )
 		{
 			if ( banker->ready_players == banker->alive_players )
